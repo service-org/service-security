0618aad89f92c21a12376180dcaf856f69ccb933
forcemain <forcemain@163.com>
2021-09-29 19:28:20 +0800

    :underage:

--- service_security/core/client/rsa/__init__.py
+++ service_security/core/client/rsa/__init__.py
@@ -22,20 +22,32 @@ class RsaClient(object):
         self.private_key = private_key or ''
         self.public_key = public_key or ''
 
-    def encrypt(self, data: bytes) -> bytes:
+    def encrypt(self, data: bytes, step: int = 100) -> bytes:
         """ 加密数据
 
+        注意: 分段加密长度不可以超过密钥长度(如1024)/8-11
+
         @param data: 原始数据
+        @param step: 加密步长
         @return: t.Text
         """
         public_obj = PKCS1_v1_5.new(RSA.importKey(self.public_key))
-        return public_obj.encrypt(data)
+        result = b''
+        for i in range(0, len(data), step):
+            result += public_obj.encrypt(data[i:i + step])
+        return result
 
-    def decrypt(self, data: bytes) -> bytes:
+    def decrypt(self, data: bytes, step: int = 128) -> bytes:
         """ 解密数据
 
+        注意: 分段解密时密钥长度1024步长是128,2046步长为256
+
         @param data: 加密数据
+        @param step: 解密步长
         @return: bytes
         """
         private_obj = PKCS1_v1_5.new(RSA.importKey(self.private_key))
-        return private_obj.decrypt(data, b'')
+        result = b''
+        for i in range(0, len(data), step):
+            result += private_obj.decrypt(data[i:i + step], b'')
+        return result
